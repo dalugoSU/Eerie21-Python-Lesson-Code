0f0acd43991cecc2aa3adf7dc12e6ca9e10578a3
Daniel Lugo <58088181+dalugoSU@users.noreply.github.com>
2021-10-26 16:00:38 -0400

    Update code.py

--- code.py
+++ code.py
@@ -1,4 +1,96 @@
-# Mad Libs!
+# Intro to python ----------------------------------------------------------------------------
+
+# <- This is a single line comment
+
+"""
+This is a
+multiline comment
+"""
+
+# Let's learn Data Types
+
+# [ Integers ] => Numbers!
+# 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+
+# [ Floats ] => Numbers with decimals!
+# 1.1, 2.2, 32.5, 6.42, 23.53
+
+# [ Booleans ] => is it true, false?
+# True, False
+
+# [ strings ] => a combination of characters!
+# "hello", "this is a sentence"
+
+# [ Character ] => a single character
+# 'a', 'v', 'c'
+
+# Our first python Code
+
+print("Hello, World")
+# What data type is "Hello, World?"
+
+# You can also print the contents of a variable
+# We will look into this more deeply soon
+
+# Variables
+
+# String
+
+
+my_school = "Syracuse University"
+print(my_school)
+
+# Variables can also hold numbers
+my_age = 22 # Python naming convention
+two = 2
+
+# You can set a variable equal to another variable
+ 
+new_number = my_age
+
+print(new_number)
+
+# You can do math with saved numbers
+another_number = my_age // two # 22 * 2 = 44
+print(another_number)
+
+# To print you use the print keyword
+print() # <= you put parenthesis
+
+# Different print methods
+
+# You can also print strings along with variables
+print("My name is:", my_age)
+print("My name is: " + my_name)
+
+# F(ormat) strings
+
+age = 22
+
+print(f"My name is: {my_name} and my age is {age}") 
+# <= can also be written like this
+
+# Saving boolean into a variable
+
+is_cold = True
+is_weekend = False
+
+print(is_weekend)
+
+# You can compare integers, and strings
+# You can use operators such as: >, <, ==, is not
+
+# 5 > 4 = True
+# 5 == 5 = True
+# 10 < 6 = False
+
+print(f"Is tesla equal to tesla: {'tesla' == 'tesla'}")
+print(f"Is 5 greater then 2: {5 > 2}")
+print(f"Is 10 less than 6: {10 < 6}")
+
+
+
+# Mad Libs! ----------------------------------------------------------------------------------
 
 # First we gather all information from our user
 
